4e6ae3d4e0e023ba07a212e1d3eeb9c6c6b30272
mario <mariobassem12@gmail.com>
2023-11-16 04:01:17 +0200

    add encoding tests
    Signed-off-by: mario <mariobassem12@gmail.com>

---
+++ encode_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"encoding/binary"
+	"encoding/json"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestEncode(t *testing.T) {
+	tests := map[string]struct {
+		input          []byte
+		compressedData []byte
+		hasError       bool
+	}{
+		"empty": {
+			input:          nil,
+			compressedData: []byte{0, 0, 0, 0},
+		},
+		"single_character": {
+			input:          []byte("a"),
+			compressedData: []byte{0, 0, 0, 1, 128},
+		},
+		"multiple_same_character": {
+			input:          []byte("aaaa"),
+			compressedData: []byte{0, 0, 0, 4, 240},
+		},
+		"multiple_differenct_characters": {
+			input:          []byte("abca"),
+			compressedData: []byte{0, 0, 0, 6, 88},
+		},
+	}
+
+	for name, tc := range tests {
+		t.Run(name, func(t *testing.T) {
+			tree := buildHuffmanTree(tc.input)
+			jsonTree, err := json.Marshal(tree)
+			assert.NoError(t, err)
+			cntBytes := make([]byte, 4)
+			binary.BigEndian.PutUint32(cntBytes, uint32(len(jsonTree)))
+
+			want := append(cntBytes, jsonTree...)
+			want = append(want, tc.compressedData...)
+
+			got, err := encode(tc.input)
+			assert.NoError(t, err)
+			assert.Equal(t, want, got)
+		})
+	}
+}
